5abec5db5bd3de2bdf9a8d549126d3f11796194d
heinhtetsoe2000 <soe991398@gmail.com>
2022-07-20 21:04:10 +0630

    section 4 update

--- section4.js
+++ section4.js
@@ -2,7 +2,57 @@ class Person {
     constructor(name) {
         this.name = name;
     }
-    getName() {
-        return this.name;
+
+    get name() {
+        return this._name;
+    }
+
+    set name(newName) {
+        newName = newName.trim();
+        if (newName === '') {
+            throw 'The name cannot be empty.';
+        }
+        this._name = newName;
     }
 }
+
+let john = new Person('John Doe');
+john.name = 'Jaden Smith';
+// console.log(john.name);
+
+let meeting = {
+    attendees: [],
+    add(attendee) {
+        // console.log(`${attendee} has joined the meeting.`);
+        this.attendees.push(attendee);
+        return this;
+    },
+    get latest() {
+        let count = this.attendees.length;
+        return count == 0 ? undefined : this.attendees[count - 1];
+    }
+};
+
+meeting.add('John').add('Samira').add('Jack');
+// console.log(`The latest attendee is ${meeting.latest}.`);
+
+function factory(aClass) {
+    return new aClass();
+}
+
+let greeting = factory(class {
+    sayHi() { console.log('Hi!') };
+})
+
+// greeting.sayHi();
+
+let app = new class {
+    constructor(name) {
+        this.name = name;
+    }
+    start() {
+        console.log(`This ${this.name} is starting.`);
+    }
+}('Awesome App');
+
+app.start();
