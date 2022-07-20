a8360d356d01e30127986ddb1863c1244712ffdb
heinhtetsoe2000 <soe991398@gmail.com>
2022-07-20 23:06:21 +0630

    section4.js finished

--- section4.js
+++ section4.js
@@ -1,5 +1,8 @@
 class Person {
     constructor(name) {
+        if(!new.target) {
+            throw "must use new operator with Person";
+        }
         this.name = name;
     }
 
@@ -14,7 +17,14 @@ class Person {
         }
         this._name = newName;
     }
+
+    static createAnonymous(gender) {
+        let name = gender === 'male' ? 'John Doe' : 'Jahn Doe';
+        return new Person(name);
+    }
 }
+
+let anonymous = Person.createAnonymous('male');
 
 let john = new Person('John Doe');
 john.name = 'Jaden Smith';
@@ -55,4 +65,86 @@ let app = new class {
     }
 }('Awesome App');
 
-app.start();
+// app.start();
+
+new class {
+    constructor(name) {
+        this.name = name;
+    }
+    start() {
+        console.log(`Starting the ${this.name}...`);
+    }
+}('New App');
+
+class Item {
+    constructor(name, quantity) {
+        this.name = name;
+        this.quantity = quantity;
+        this.constructor.count++;
+    }
+    static count = 0;
+
+    static get count() {
+        return Item.count++;
+    }
+}
+
+let pen = new Item('Pen', 5);
+let noteBook = new Item('Note Book', 2);
+
+// console.log(Item.count);
+
+let propName = 'c';
+
+const rank = {
+    a: 1,
+    b: 2,
+    [propName]: 3,
+};
+
+// console.log(rank.c);
+
+let name = 'fullName';
+
+class Customer {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    get [name]() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+let customer = new Customer('Tyler', 'One');
+// console.log(customer.fullName);
+
+class Animal {
+    constructor(legs) {
+        this.legs = legs;
+    }
+    walk() {
+        console.log(`Walking on ${this.legs} legs.`);
+    }
+}
+
+class Bird extends Animal {
+    constructor(legs, color) {
+        super(legs);
+        this.color = color;
+    }
+    fly() {
+        console.log(`Flying...`)
+    }
+    get color() {
+        return this._color;
+    }
+    set color(color) {
+        this._color = color;
+    }
+}
+
+let bird = new Bird(2, 'brown');
+bird.walk();
+console.log(bird.color);
+
